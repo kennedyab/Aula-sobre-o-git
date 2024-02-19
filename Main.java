b571402e8d8d653454cad5ae3900585b11fe6f24
kennedyab <kennedy.barbosa@ccc.ufcg.edu.br>
2024-02-19 10:15:18 -0300

    meu primeiro commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+//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
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("");
+		System.out.println("Hello, World!");
+	}
+}
+
